afd64617e44110a16bc92fb780c7094b28385d76
Kerim Campbell <kerim.me@icloud.com>
2023-03-30 13:54:38 +0100

    register working, login not

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,48 +3,44 @@ import axios from 'axios';
 import async from "async";
 
 const Login = ({ onLogin }) => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
-    const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-        const response = await axios.post('http://localhost:5001/login', {
-            username, password,
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const response = await axios.post("http://localhost:5001/login", {
+            username,
+            password,
         });
         if (response.status === 200) {
-            alert('Login successful');
-            onLogin(response.data.username);
+            const user = response.data.user;
+            onLogin(user);
+        } else {
+            alert("Invalid credentials");
         }
-    } catch (error) {
-        // console.error( 'Error logging in:', error);
-        alert('Invalid credentials');
-    }
-};
+    };
 
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
             <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Username:</label>
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type= "password"
-                        value= {password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
+            <label>
+                Username:
+                <input
+                    type="text"
+                    value={username}
+                    onChange={(event) => setUsername(event.target.value)}
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
+                />
+            </label>
+            <button type="submit">Submit</button>
+        </form>
     );
 };
 
